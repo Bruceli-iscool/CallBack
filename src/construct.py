85231a7162cdbe0b9caf858b3bc84c6585a1647d
Bruce Li <isanoob1897@outlook.com>
2024-03-25 15:30:15 -0700

    add

--- src/construct.py
+++ src/construct.py
@@ -3,9 +3,10 @@
 parens = 0
 render = ""
 stateparens = 0
+
 def construct(line):
     global parens, render, stateparens
-    line = line.lstrip()
+    line = line.lstrip(" ")
     if parens == 0:
         if line.startswith("render "):
             # HTML rendering
@@ -21,7 +22,7 @@ def construct(line):
             else:
                 parse(line, 1)
     else:
-        if "(" in line and stateparens == 0:
+        if ")" in line and stateparens == 0:
             parens = parens - 1
             x = "\t</body>\n" if render == "body" else "\t</head>\n"
             return x
@@ -29,7 +30,7 @@ def construct(line):
             return parse(line, 2)
 def parse(line, mode):
     if mode == 2:
-        if line.startswith("p(") and render == "body":
+        if line.startswith("p{") and render == "body":
             line = line.replace("p{", "")
             line = line.replace("}", "")
             return "<p>" + line + "</p>"
