4719296086db87e6fa6b1d7570ff9ebb9cd8d4fb
Bruce Li <isanoob1897@outlook.com>
2024-03-25 16:03:37 -0700

    add

--- src/compile.py
+++ src/compile.py
@@ -1,14 +1,14 @@
 import construct
+import webbrowser
 
-def compile(file):
+def compile(file, out):
     final = "<!DOCTOTYPE html>\n\t<html>\n"
     with open(file) as file:
         for line in file:
             if line.startswith("//"):
                 # ignore comments
                 pass
-            z = construct.construct(line)
-            print(z)
+            z = construct.construct_html(line)
             final = final + z
         return final + "\n</html>"
 
@@ -16,7 +16,8 @@ def start():
     return "<!DOCTOTYPE html>\n\t<html>\n"
 
 def out(inputFile, file="out.html"):
-    with open(file, "w") as file:
-        file.write(compile(inputFile))
+    with open(file, "w") as files:
+        files.write(compile(inputFile, files))
+        webbrowser.open(file)
 
 out("/Users/keli/Documents/GitHub/Lascal/CallBack/src/test.cb")
--- src/construct.py
+++ src/construct.py
@@ -3,8 +3,10 @@
 parens = 0
 render = ""
 stateparens = 0
+# save type
+var = {}
 
-def construct(line):
+def construct_html(line):
     global parens, render, stateparens
     line = line.lstrip(" ")
     if parens == 0:
@@ -20,15 +22,15 @@ def construct(line):
                 render = "body"
                 return "\t<body>\n"
             else:
-                parse(line, 1)
+                parse_html(line, 1)
     else:
         if ")" in line and stateparens == 0:
             parens = parens - 1
             x = "\t</body>\n" if render == "body" else "\t</head>\n"
             return x
         else:
-            return parse(line, 2)
-def parse(line, mode):
+            return parse_html(line, 2)
+def parse_html(line, mode):
     if mode == 2:
         if line.startswith("p{") and render == "body":
             line = line.replace("p{", "")
@@ -38,3 +40,33 @@ def parse(line, mode):
             return ""
     else:
         return ""
+def parse(line):
+    # CallBack parsing
+    # syntax: let int x = 5;
+    line = line.replace(";", "")
+    if line.lstrip().startswith("let"):
+        if " int " in line:
+            modified_line = line.replace("let int ", "")
+            name, modified_line = modified_line.split("=")
+            name = name.replace(" ", "")
+            if checkType(modified_line, int):
+                var[name] = int
+                return line.replace(" int", "")
+            else:
+                print(f"ValueError!: {modified_line} is not compatiable with int!")
+        if " String " in line:
+            modified_line = line.replace("let String ", "")
+            name, modified_line = modified_line.split("=")
+            name = name.replace(" ", "")
+            if checkType(modified_line, str):
+                var[name] = str
+                return line.replace(" String", "")
+            else:
+                print(f"ValueError!: {modified_line} is not compatiable with String!")
+# Check type
+def checkType(value, type):
+        x = isinstance(value, type)
+        return x
+
+parse("let String hi = 5")
+print(var)
